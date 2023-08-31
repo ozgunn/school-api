f8d699fa4b8e5a9c152c2548dabcb6514e17430e
Ozgun <ozgunn@gmail.com>
2023-08-31 23:29:59 +0300

    added localization middleware

---
+++ app/Http/Middleware/Localization.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Localization
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $local = ($request->hasHeader('X-locale')) ? $request->header('X-locale') : 'en';
+        app()->setLocale($local);
+        return $next($request);
+    }
+}
